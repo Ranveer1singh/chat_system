de4291e85d0f3be29419f1ff03d83277477b659c
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-09-15 23:31:56 +0530

    update : setup consumer for socket

--- socket_service/src/index.ts
+++ socket_service/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express"
-
+import {startConsumer } from "./kafka/consumer"
 const app = express()
 
 
-app.listen(3002, ()=>{
+app.listen(3002,async ()=>{
+await startConsumer()
     console.log("socket is running on 3002")
 })
---
+++ socket_service/src/kafka/consumer.ts
@@ -0,0 +1,37 @@
+import { Kafka } from "kafkajs";
+
+// 1. Create Kafka client
+const kafka = new Kafka({
+  clientId: "socket-service", // identify this service
+  brokers: [process.env.KAFKA_BROKER || "localhost:9092"], // from .env
+});
+
+// 2. Create a consumer (give groupId)
+const consumer = kafka.consumer({ groupId: "socket-service-group" });
+
+export const startConsumer  = async () => {
+  // 3. Connect
+  await consumer.connect();
+  console.log("✅ Kafka Consumer connected");
+
+  // 4. Subscribe to topic
+  await consumer.subscribe({ topic: "chat-messages", fromBeginning: true });
+  console.log("📩 Subscribed to topic: chat-messages");
+
+  // 5. Run consumer loop
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const key = message.key?.toString();
+      const value = message.value?.toString();
+
+      console.log(
+        `📥 Received message [${topic} | partition ${partition}] key=${key} value=${value}`
+      );
+
+      // TODO: here you can emit via socket.io / ws
+      // Example: io.to(key /*chatId*/).emit("message", JSON.parse(value));
+    },
+  });
+};
+
+// run().catch(console.error);
