813e17772861de9278bc9fa6299daac82f786139
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-09-29 22:23:25 +0530

    udpate : socket server

--- socket_service/src/index.ts
+++ socket_service/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import {startConsumer } from "./kafka/consumer"
+import { initSocket } from "./socket";
 const app = express()
 
 
 app.listen(3002,async ()=>{
 await startConsumer()
     console.log("socket is running on 3002")
+
+    initSocket(app)
 })
---
+++ socket_service/src/socket.ts
@@ -0,0 +1,34 @@
+import { Server, Socket } from "socket.io";
+import { Application } from "express";
+import http, { Server as HttpServer } from "http";
+import { AuthenticatedSocket, authenticateSocket } from "./middleware/authenticate";
+
+let io: Server | null = null;
+let httpServer: HttpServer | null = null;
+
+export const initSocket = (app: Application): void => {
+  // Create HTTP server from express app
+  httpServer = http.createServer(app);
+
+  io = new Server(httpServer, {
+    cors: {
+      origin: "*", // restrict to your frontend domain in production
+    },
+  });
+  io.use(authenticateSocket);
+
+ io.on("connection", (socket: AuthenticatedSocket) => {
+    console.log("✅ Client connected:", socket.id, "User:", socket.user);
+
+    socket.on("disconnect", () => {
+      console.log("❌ Client disconnected:", socket.id);
+    });
+  });
+
+  httpServer.listen(4000, () => {
+    console.log("Socket server running on 4000");
+  });
+};
+
+// Export for other files to emit events
+export const getIO = (): Server | null => io;
