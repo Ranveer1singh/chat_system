429aca9e549f9f092032fb27a73aaaabf52fc152
ranveer1singh <ranveersinghtomar2000@gmail.com>
2026-01-11 16:12:55 +0530

    update :-lord icon

--- client_service/src/component/lord-icon/AnimatedChatIcon.tsx
+++ client_service/src/component/lord-icon/AnimatedChatIcon.tsx
@@ -1,13 +1,13 @@
 
 const AnimatedChatIcon = () => {
   return (
-    <lord-icon
-      src="https://cdn.lordicon.com/jdgfsfzr.json"
-      trigger="loop"
-      delay="2000"
-      colors="primary:#2D6936,secondary:#A5D6A7" 
-      style={{ width: '250px', height: '250px' }}
-    />
+    // <lord-icon
+    //   src="https://cdn.lordicon.com/jdgfsfzr.json"
+    //   trigger="loop"
+    //   delay="2000"
+    //   colors="primary:#2D6936,secondary:#A5D6A7" 
+    //   style={{ width: '250px', height: '250px' }}
+    // />
   );
 }
 
