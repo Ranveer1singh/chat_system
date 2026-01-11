d4265aee4c1207acb7be9b18bff02e6e09207d62
ranveer1singh <ranveersinghtomar2000@gmail.com>
2026-01-11 16:14:29 +0530

    update :- chnage in lord icon

--- client_service/src/component/lord-icon/AnimatedChatIcon.tsx
+++ client_service/src/component/lord-icon/AnimatedChatIcon.tsx
@@ -1,14 +1,17 @@
 
 const AnimatedChatIcon = () => {
   return (
-    // <lord-icon
+    <div className="">
+    
+    </div>
+  );
+}
+
+export default AnimatedChatIcon
+// <lord-icon
     //   src="https://cdn.lordicon.com/jdgfsfzr.json"
     //   trigger="loop"
     //   delay="2000"
     //   colors="primary:#2D6936,secondary:#A5D6A7" 
     //   style={{ width: '250px', height: '250px' }}
     // />
-  );
-}
-
-export default AnimatedChatIcon
