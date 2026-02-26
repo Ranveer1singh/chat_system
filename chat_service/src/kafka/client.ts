98fb294117983beb6d2e062bf86400906961f145
ranveer1singh <ranveersinghtomar2000@gmail.com>
2026-02-26 11:07:49 +0530

    Setup new changes for the project

--- chat_service/src/kafka/client.ts
+++
@@ -1,7 +0,0 @@
-// kafkaClient.ts
-import { Kafka } from "kafkajs";
-
-export const kafka = new Kafka({
-  clientId: "chat-service",
-  brokers: [process.env.KAFKA_BROKER || "localhost:9092"],
-});
--- chat_service/src/routes/messageroutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { chatController } from "../controller/chat"
-import { messageController } from "../controller/message"
-
-const router = express.Router()
-
-router.post("/" , messageController.sendMessage)
-
-export default router
